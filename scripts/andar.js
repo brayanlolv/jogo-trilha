8d2122d6e4d2012a62d9502056059326b56d1055
brayan <brayanlucasop@gmail.com>
2023-10-27 22:10:22 -0300

    comecei a fazer a tela

--- scripts/andar.js
+++ scripts/andar.js
@@ -1,4 +1,4 @@
-let position = [2,4]
+let position = [2, 4]
 
 console.log(position);
 const m = position[0] - 1
@@ -7,7 +7,7 @@ const n = position[1] - 1
 
 function andar(m, n) {
 
-let result = []
+    let result = []
 
     // testar m
     let l1, l2
@@ -19,7 +19,6 @@ let result = []
             l1 < 7 ? result.push([m, l1]) : null
             l2 = n - 3
             l2 > -1 ? result.push([m, l2]) : null
-            console.log("linha das pontas")
             break
 
         case (1):
@@ -29,7 +28,14 @@ let result = []
             l2 = n - 2
             l1 < 6 ? result.push([m, l1]) : null
             l2 > 0 ? result.push([m, l2]) : null
-            console.log("linha medianas")
+            break
+
+        case (2):
+        case (4):
+            l1 = n + 1
+            l2 = n - 1
+            l1 < 5 ? result.push([m, l1]) : null
+            l2 > 1 ? result.push([m, l2]) : null
             break
 
         case (3):
@@ -38,8 +44,7 @@ let result = []
             l2 = n - 1
 
             if (l1 < 7 && l1 != 3) { result.push([m, l1]) }
-            if (l2 > -1 && l2!=3) { result.push([m, l2]) }
-            console.log("linha bem do meio")
+            if (l2 > -1 && l2 != 3) { result.push([m, l2]) }
             break
     }
 
@@ -53,7 +58,6 @@ let result = []
             l1 < 7 ? result.push([l1, n]) : null
             l2 = m - 3
             l2 > -1 ? result.push([l2, n]) : null
-            console.log("linha das pontas")
             break
 
 
@@ -62,39 +66,39 @@ let result = []
 
             l1 = m + 2
             l2 = m - 2
-            l1 < 6 ? result.push([l1, n]) :null
+            l1 < 6 ? result.push([l1, n]) : null
             l2 > 0 ? result.push([l2, n]) : null
-            console.log("linha medianas")
             break
 
-            case (3):
+        case (2):   
+        case (4):
+            l1 = m+ 1
+            l2 = m- 1
+            l1 < 5 ? result.push([l1,n]) :null
+            l2 > 1 ? result.push([l2, n]) : null
+            
+
+            break
+
+        case (3):
 
             l1 = m + 1
             l2 = m - 1
-
-            if (l1 < 7 && l1 != 3) { result.push([l1,n]) }
-            if (l2 > -1 && l2!=3) { result.push([l2,n]) }
-            console.log("linha bem do meio")
+            if (l1 < 7 && l1 != 3) { result.push([l1, n]) }
+            if (l2 > -1 && l2 != 3) { result.push([l2, n]) }
             break
     }
 
     return result
 }
 
-
-
-function  includeArray(arrayPai,m,n){
+function includeArray(arrayPai, m, n) {
     let incluso = false
-    arrayPai.map((element)=>{
-        if(element[0]==m
-            &&element[1]==n){incluso = true}
+    arrayPai.map((element) => {
+        if (element[0] == m
+            && element[1] == n) { incluso = true }
     })
 
     return incluso
- }
-
-
-console.log("posicoes vizinhas")
-
-//implementar isso na pratica
-export {andar, includeArray}
+}
+export { andar, includeArray }
